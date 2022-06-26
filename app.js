b37c929136fa48a30a9ecab4732688a6f8523f52
Oscar Zhang <oscarzhang228@gmail.com>
2022-06-25 22:21:30 -0400

    write story and rules and allow click to skip transition

--- app.js
+++ app.js
@@ -32,3 +32,37 @@ function finish() {
   clearInterval(timer);
   timer = null;
 }
+
+const rules = document.querySelector(".rules");
+const rulesArr = rules.textContent.split("");
+
+rules.textContent = "";
+
+for (let i = 0; i < rulesArr.length; i++) {
+  rules.innerHTML += "<span>" + rulesArr[i] + "</span>";
+}
+
+setTimeout(() => {
+  let index = 0;
+  let timer2 = setInterval(transition, 100);
+
+  function transition() {
+    const theSpan = rules.querySelectorAll("span")[index];
+    theSpan.classList.add("fade");
+    index++;
+    if (index === rulesArr.length) {
+      complete();
+      return;
+    }
+  }
+  function complete() {
+    clearInterval(timer2);
+    timer2 = null;
+  }
+  window.addEventListener("click", () => {
+    const allSpan = document.querySelectorAll("span");
+    allSpan.forEach((text) => {
+      text.style.cssText = "opacity:1;";
+    });
+  });
+}, 10500);
